f516e7021cdf328efb539a20a25f22bdc81898dd
Will Banfield <william.banfield@10gen.com>
2017-08-16 14:28:57 -0400

    WIP

--- mongoreplay/tester.go
+++ mongoreplay/tester.go
@@ -1,42 +1,46 @@
 package mongoreplay
 
 import (
-	"fmt"
-	"io"
+	mgo "github.com/10gen/llmgo"
 )
 
 // TesterCommand stores settings for the mongoreplay 'test' subcommand
 type TesterCommand struct {
-	GlobalOpts   *Options `no-flag:"true"`
-	PlaybackFile string   `description:"path to the playback file to play from" short:"p" long:"playback-file" required:"yes"`
+	GlobalOpts *Options `no-flag:"true"`
+	URL        string   `short:"h" long:"host" description:"Location of the host to play back against" default:"mongodb://localhost:27017"`
 	StatOptions
 }
 
 func (tester *TesterCommand) Execute(args []string) error {
-	fmt.Println("hello")
-	playbackFileReader, err := NewPlaybackFileReader(tester.PlaybackFile, false)
-	if err != nil {
-		return err
+	conns := 4000
+	sessions := make([]*mgo.Session, conns)
+
+	for i := 0; i < conns; i++ {
+		session, err := mgo.Dial(tester.URL)
+		if err != nil {
+			panic(err)
+		}
+		sessions[i] = session
 	}
-
-	var opChan <-chan *RecordedOp
-	var errChan <-chan error
-
-	opChan, errChan = NewOpChanFromFile(playbackFileReader, 1)
-
-	for op := range opChan {
-		fmt.Println(op)
-		/*
-
-			put the relevant test thing to do here
-
-		*/
-
+	for _, session := range sessions {
+		session.Close()
 	}
-	//handle the error from the errchan
-	err = <-errChan
-	if err != nil && err != io.EOF {
-		userInfoLogger.Logvf(Always, "OpChan: %v", err)
+	return nil
+}
+
+func (tester *TesterCommand) Execute(args []string) error {
+	conns := 4000
+	sessions := make([]*mgo.Session, conns)
+
+	for i := 0; i < conns; i++ {
+		socket, err := session.AcquireT(tester.URL)
+		if err != nil {
+			panic(err)
+		}
+		sessions[i] = session
+	}
+	for _, session := range sessions {
+		session.Close()
 	}
 	return nil
 }
