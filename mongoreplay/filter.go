269c27fa31e34ae06c1371aca47b243310875198
Will Banfield <william.banfield@10gen.com>
2017-08-31 10:56:45 -0400

    split phase

--- mongoreplay/filter.go
+++ mongoreplay/filter.go
@@ -1,7 +1,10 @@
 package mongoreplay
 
 import (
+	"fmt"
 	"io"
+	"os"
+	"sync"
 
 	"github.com/10gen/llmgo/bson"
 )
@@ -10,7 +13,9 @@ import (
 type FilterCommand struct {
 	GlobalOpts      *Options `no-flag:"true"`
 	PlaybackFile    string   `description:"path to the playback file to read from" short:"p" long:"playback-file" required:"yes"`
-	OutFile         string   `description:"path to the output file to write to" short:"o" long:"output-file"`
+	OutFile         string   `description:"path to the output file to write to" short:"o" long:"outputFile"`
+	SplitFilePrefix string   `description:"prefix file name to use for the output files being written when splitting traffic" long:"outfilePrefix"`
+	Split           int      `description:"split the traffic into n files with roughly equal numbers of connecitons in each" default:"1" long:"split"`
 	RemoveDriverOps bool     `description:"remove driver issued operations from the playback" long:"removeDriverOps"`
 	Gzip            bool     `long:"gzip" description:"decompress gzipped input"`
 }
@@ -29,14 +34,26 @@ func (filter *FilterCommand) Execute(args []string) error {
 	}
 	opChan, errChan := NewOpChanFromFile(playbackFileReader, 1)
 
-	// now write to a new file
-	playbackWriter, err := NewPlaybackWriter(filter.OutFile, filter.Gzip)
-	if err != nil {
-		return err
+	outfiles := make([]*PlaybackWriter, filter.Split)
+	if filter.Split == 1 {
+		playbackWriter, err := NewPlaybackWriter(filter.OutFile, filter.Gzip)
+		if err != nil {
+			return err
+		}
+		outfiles[0] = playbackWriter
+	} else {
+		for i := 0; i < filter.Split; i++ {
+			playbackWriter, err := NewPlaybackWriter(
+				fmt.Sprintf("%s%02d.playback", filter.SplitFilePrefix, i), filter.Gzip)
+			if err != nil {
+				return err
+			}
+			outfiles[i] = playbackWriter
+			defer playbackWriter.Close()
+		}
 	}
-	defer playbackWriter.Close()
 
-	if err := Filter(opChan, playbackWriter, filter.RemoveDriverOps); err != nil {
+	if err := Filter(opChan, outfiles, filter.RemoveDriverOps); err != nil {
 		userInfoLogger.Logvf(Always, "Play: %v\n", err)
 	}
 
@@ -47,7 +64,19 @@ func (filter *FilterCommand) Execute(args []string) error {
 	}
 	return nil
 }
-func Filter(opChan <-chan *RecordedOp, playbackWriter *PlaybackWriter, removeDriverOps bool) error {
+
+func Filter(opChan <-chan *RecordedOp,
+	outfiles []*PlaybackWriter,
+	removeDriverOps bool) error {
+
+	opWriters := make([]chan<- *RecordedOp, len(outfiles))
+	errChan := make(chan error)
+	wg := &sync.WaitGroup{}
+
+	for i := range outfiles {
+		opWriters[i] = newParallelPlaybackWriter(outfiles[i], errChan, wg)
+	}
+
 	for op := range opChan {
 		if removeDriverOps {
 			parsedOp, err := op.RawOp.Parse()
@@ -58,18 +87,75 @@ func Filter(opChan <-chan *RecordedOp, playbackWriter *PlaybackWriter, removeDri
 				continue
 			}
 		}
-		bsonBytes, err := bson.Marshal(op)
-		if err != nil {
-			return err
-		}
-		_, err = playbackWriter.Write(bsonBytes)
-		if err != nil {
-			return err
-		}
+		// Determine which one to write it to
+		fileNum := op.SeenConnectionNum % int64(len(outfiles))
+		opWriters[fileNum] <- op
 	}
+
+	for _, opWriter := range opWriters {
+		close(opWriter)
+	}
+	wg.Wait()
+	close(errChan)
+
+	var hasError bool
+	for err := range errChan {
+		hasError = true
+		userInfoLogger.Logvf(Always, "error: %s", err)
+	}
+	if hasError {
+		return fmt.Errorf("errors encountered while running filter")
+	}
+
 	return nil
 }
 
+func newParallelPlaybackWriter(outfile *PlaybackWriter,
+	errChan chan<- error, wg *sync.WaitGroup) chan<- *RecordedOp {
+	var didWriteOp bool
+
+	inputOpChan := make(chan *RecordedOp, 1000)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for op := range inputOpChan {
+			bsonBytes, err := bson.Marshal(op)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			_, err = outfile.Write(bsonBytes)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			didWriteOp = true
+		}
+		if !didWriteOp {
+			userInfoLogger.Logvf(Always, "no connections written to file %s, removing", outfile.fname)
+			err := os.Remove(outfile.fname)
+			if err != nil {
+				errChan <- err
+				return
+			}
+		}
+	}()
+	return inputOpChan
+}
+
 func (filter *FilterCommand) ValidateParams(args []string) error {
+	if filter.Split < 1 {
+		return fmt.Errorf("must be a positive number of files to split into")
+	}
+	if filter.Split > 1 && filter.SplitFilePrefix == "" {
+		return fmt.Errorf("must specify a filename prefix when splitting traffic")
+	}
+	if filter.Split > 1 && filter.OutFile != "" {
+		return fmt.Errorf("must not specify an output file name when splitting traffic" +
+			"instead only specify a file name prefix")
+	}
+	if filter.Split == 1 && filter.OutFile == "" {
+		return fmt.Errorf("must specify an output file")
+	}
 	return nil
 }
